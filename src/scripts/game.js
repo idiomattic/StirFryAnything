1325c4bc5697bc255e79955296ea467bd07727a9
Matthew Lese <matthewjlese@gmail.com>
2021-10-31 14:21:48 -0700

    refactors background rendering into space.js

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -14,39 +14,39 @@ export default class Game {
   }
   
   render() {
-    // context is canvas object here and within forEach
-    const startStars = this.generateStars(50);
-    startStars.forEach((star) => {
-      star.drawStars(this.ctx);
-    })
+    // const startStars = this.generateStars(1);
+    // startStars.forEach((star) => {
+    //   star.drawStar(this.ctx);
+    // })
+    Star.render.bind(this)();
   }
 
-  generateStars(n) {
-    const stars = [];
-    for (let i = 0; i < n; i++) {
-      let posish = generateStarPosish.call(this.ctx)
-      stars.push(new Star(this.ctx, posish, (5 + 10 * Math.random())))
-    }
-    return(stars)
-  }
+  // generateStars(n) {
+  //   const stars = [];
+  //   for (let i = 0; i < n; i++) {
+  //     let posish = generateStarPosish.call(this.ctx)
+  //     stars.push(new Star(this.ctx, posish, (5 + 10 * Math.random())))
+  //   }
+  //   return(stars)
+  // }
 
 }
 
-function generateStarPosish() {
-  let width = this.canvas.width;
-  let height = this.canvas.height;
-  let bigRandy = Math.random(); // randy is the boss
-  let x = null;
-  let y = null;
-  if (bigRandy < 0.5) {
-    x = (width  * 0.2) + (width * 0.6) * Math.random();
-    let littleRandy = Math.random();
-    y = littleRandy < 0.5 ? height * 0.2 : height * 0.8;
-  } else {
-    y = (height  * 0.2) + (height * 0.6) * Math.random();
-    let littleRandy = Math.random();
-    x = littleRandy < 0.5 ? width * 0.2 : width * 0.8;
-  }
-  return [x, y]
-}
+// function generateStarPosish() {
+//   let width = this.canvas.width;
+//   let height = this.canvas.height;
+//   let bigRandy = Math.random(); // randy is the boss
+//   let x = null;
+//   let y = null;
+//   if (bigRandy < 0.5) {
+//     x = (width  * 0.2) + (width * 0.6) * Math.random();
+//     let littleRandy = Math.random();
+//     y = littleRandy < 0.5 ? height * 0.2 : height * 0.8;
+//   } else {
+//     y = (height  * 0.2) + (height * 0.6) * Math.random();
+//     let littleRandy = Math.random();
+//     x = littleRandy < 0.5 ? width * 0.2 : width * 0.8;
+//   }
+//   return [x, y]
+// }
 
--- src/scripts/space.js
+++ src/scripts/space.js
@@ -1,3 +1,5 @@
+import Game from './game'
+
 const COLORS = [
   'FFFFFF',
   'F5F5F5',
@@ -23,11 +25,45 @@ export default class Star {
     this.color = COLORS[Math.floor(Math.random()*COLORS.length)];
     
   }
-
+  
   //arg ctx comes from this.ctx in game.js
-  drawStars(ctx) {
+  drawStar(ctx) {
     ctx.fillStyle = '#' + `${this.color}`
     ctx.fillRect(this.x, this.y, this.size, this.size);
+    // this.generateStarPosish()
   }
+  
+  generateStarPosish() {
+    let width = this.canvas.width;
+    let height = this.canvas.height;
+    let randy = Math.random();
+    let x = null;
+    let y = null;
+    if (randy < 0.5) {
+      x = (width * 0.2) + Math.random() * (width * 0.6);
+      y = Math.random() < 0.5 ? height * 0.2 : height * 0.8;
+    } else {
+      y = (height * 0.2) + Math.random() * (height * 0.6);
+      x = Math.random() < 0.5 ? width * 0.2 : width * 0.8;
+    }
+    return [x, y];
+    console.log(this);
+  }
+  
 
+  generateStars(n) {
+    const stars = [];
+    for (let i = 0; i < n; i++) {
+      let posish = Star.prototype.generateStarPosish.bind(this)();
+      stars.push(new Star(this.canvas, posish, (5 + 10 * Math.random())))
+    }
+    return stars;
+  }
+  
+  static render() {
+    const startStars = Star.prototype.generateStars.bind(this)(10);
+    startStars.forEach((star) => {
+      star.drawStar(this.ctx);
+    }, this)
+  }
 }

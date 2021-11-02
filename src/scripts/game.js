5f19cbcedf0789e4fdce984154332b39e23bc75e
Matthew Lese <matthewjlese@gmail.com>
2021-11-02 16:29:59 -0700

    toggle items between lists, sizes images near-appropriately

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -26,12 +26,25 @@ export default class Game {
 
   level1() {
     if (this.level !== 1) { this.startGame() };
-    let firstDirs = printer('Click ingredients to add them to your tray')
+    let firstDirs = printer('Click ingredients to add them to your tray');
     firstDirs.style.top = "23%";
     const container = document.getElementById('game-container');
     this.buildTray(container)
-    this.buildLists(container);
-
+    let lists = this.buildLists(container);
+    let numVeggiesLeft = lists[0].getElementsByTagName('li').length;
+    let numProteinsLeft = lists[1].getElementsByTagName('li').length;
+    // if (numVeggiesLeft < 4) {
+    //   let error = printer('Too many vegetables!');
+    //   error.style.top = '23%';
+    //   error.style.color = 'red';
+    //   error.style.fontWeight = '100';
+    // }
+    // if (numProteinsLeft < 2) {
+    //   let error = printer('Save some protein for the rest of us!');
+    //   error.style.top = '23%';
+    //   error.style.color = 'red';
+    //   error.style.fontWeight = '100';  
+    // }
   }
 
   buildTray(container) {
@@ -49,8 +62,12 @@ export default class Game {
 
   togglePos() {
     const chosenList = document.querySelector('#chosen-ingredients');
-    const parent = this.classList.contains('Vegetable') ? 
-      document.querySelector('.veggieList') : document.querySelector('.proteinList');
+    let parent = undefined;
+    if (this.classList.contains('Vegetable')) {
+      parent = document.getElementById('veggieList');
+    } else {
+      parent = document.getElementById('proteinList');
+    }
     if (!this.classList.contains('chosen')) {
       parent.removeChild(this);
       this.classList.toggle('chosen');
@@ -65,7 +82,8 @@ export default class Game {
   buildLists(container) {
     // const container = document.getElementById('game-container');
     let veggieList = document.createElement('ul');
-    veggieList.setAttribute('class', 'veggieList');
+    veggieList.setAttribute('id', 'veggieList');
+    container.appendChild(veggieList);
     vegetables.forEach((veggie) => {
       let veggieEl = document.createElement('li');
       veggieEl.setAttribute('class', `${veggie.category}`)
@@ -73,14 +91,14 @@ export default class Game {
       let veggieImg = document.createElement('img');
       veggieImg.setAttribute('src', `${veggie.raw_img_link}`);
       veggieImg.setAttribute('alt', `${veggie.name}`);
-      veggieImg.addEventListener('click', this.togglePos.bind(veggieEl)());
+      veggieImg.addEventListener('click', this.togglePos.bind(veggieEl));
       veggieEl.appendChild(veggieImg);
       veggieList.appendChild(veggieEl);
     })
-    container.appendChild(veggieList);
     
     let proteinList = document.createElement('ul');
-    proteinList.setAttribute('class', 'proteinList');
+    proteinList.setAttribute('id', 'proteinList');
+    container.appendChild(proteinList);
     proteins.forEach((protein) => {
       let proteinEl = document.createElement('li');
       proteinEl.setAttribute('class', `${protein.category}`)
@@ -88,12 +106,10 @@ export default class Game {
       let proteinImg = document.createElement('img');
       proteinImg.setAttribute('src', `${protein.raw_img_link}`);
       proteinImg.setAttribute('alt', `${protein.name}`);
-      proteinImg.addEventListener('click', function(e) {
-        console.log('clicked prot');
-      });
+      proteinImg.addEventListener('click', this.togglePos.bind(proteinEl));
       proteinEl.appendChild(proteinImg);
       proteinList.appendChild(proteinEl);
     })
-    container.appendChild(proteinList);
+    return [veggieList, proteinList];
   }
 }
